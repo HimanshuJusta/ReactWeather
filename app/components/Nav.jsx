7f00723b2150051bc4d9f40176293880c29bcde5
Himanshu Justa <himanshu.justa@accenture.com>
2016-07-21 12:14:33 +0530

    Styled the Nav UI

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
 import { Link, IndexLink } from 'react-router';
 
-export default (props) =>{
-  return(
-    <div>
-      <h2>Nav Component</h2>
-      <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-      <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
-      <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Example</Link>
-    </div>
-  );
+export default class Nav extends Component{
+  constructor(props){
+    super(props);
+    this.onSearch = this.onSearch.bind(this);
+  }
+  onSearch(e){
+    e.preventDefault();
+    alert('Not Yet Wired Up');
+  }
+  render(){
+    return(
+      <div className="top-bar">
+          <div className="top-bar-left">
+            <ul className="menu">
+              <li className="menu-text">React Weather App</li>
+              <li>
+                <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+              </li>
+              <li>
+                <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
+              </li>
+              <li>
+                <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Example</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="top-bar-right">
+            <form onSubmit={this.onSearch}>
+              <ul className="menu">
+                <li><input type="search" placeholder="Search Weather"/></li>
+                <li><button type="button" className="button">Get Weather</button></li>
+              </ul>
+            </form>
+          </div>
+      </div>
+    );
+  }
 }
