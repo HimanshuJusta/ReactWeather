3602def8cacdaef0a0ac18cb6bdba82d79927d6c
Himanshu Justa <himanshu.justa@accenture.com>
2016-07-21 13:23:49 +0530

    Styling Main and Example Component

--- app/components/Example.jsx
+++ app/components/Example.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router';
 export default (props) => {
   return(
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to Examples Page</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Heare are few example locations to try out:</p>
+      <ol>
+        <li><Link to='/?location=Philadelphia'>Philadelphia, PA</Link></li>
+        <li><Link to='/?location=Rio'>Rio, Brazil</Link></li>
+      </ol>
     </div>
   );
 }
