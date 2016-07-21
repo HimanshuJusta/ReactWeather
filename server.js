69ed13eb9333370fc9fa148166562c2d8d681a54
Himanshu Justa <himanshu.justa@accenture.com>
2016-07-21 09:34:14 +0530

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 var express = require('express');
 
 var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(function(req, resp, next){
+    if(req.headers['x-forwarded-proto'] === 'https'){
+      resp.redirect('http://' + req.hostname + req.url);
+    }else{
+      next();
+    }
+})
+
 
 app.use(express.static('public'));
 
-app.listen(3000, function(){
-  console.log('Express sever is up on port 3000');
+app.listen(PORT, function(){
+  console.log('Express sever is up on port '+PORT);
 });
